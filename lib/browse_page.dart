8e421432c9e2a025af917478c6d4b8fbf54e8de7
x1a0min9 <xiaominglui@gmail.com>
2023-04-05 06:54:43 +0800

    auto login key step works

--- lib/browse_page.dart
+++ lib/browse_page.dart
@@ -14,10 +14,29 @@ class _BrowsePageState extends State<BrowsePage> {
   final _url = 'https://m.zmxyj.com/login/index';
   WebViewController? _webViewController;
   int _currentAccountIndex = -1;
+  final _accountProcessedController = StreamController<void>.broadcast();
+
+
+  @override
+  void dispose() {
+    _accountProcessedController.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final accountProvider = Provider.of<AccountProvider>(context);
+
+    Future<void> _logout() async {
+      await _webViewController?.evaluateJavascript('''
+        (function() {
+          document.getElementsByClassName("header-message-t back")[2].click();
+          setTimeout(function() {
+            document.querySelector("body > div.modal.modal-in > div.modal-buttons > span.modal-button.modal-button-bold").click();
+          }, 500);
+        })();
+      ''');
+    }
 
     Future<void> _loginWithAccount(Account account) async {
       String phoneNumber = account.phoneNumber;
@@ -37,16 +56,28 @@ class _BrowsePageState extends State<BrowsePage> {
     }
 
     void _autoLogin() async {
+      print("autoLogin");
       for (int i = 0; i < accountProvider.accounts.length; i++) {
         _currentAccountIndex = i;
+        print("autoLogin---${i}---${_currentAccountIndex}");
         await _loginWithAccount(accountProvider.accounts[i]);
-        await Future.delayed(Duration(seconds: 1));
+        print("autoLogin---waiting---${_currentAccountIndex}");
+        await _accountProcessedController.stream.first;
       }
     }
 
-    void _onPageFinished(String url) {
+    void _onPageFinished(String url) async {
+      print("onPageFinished");
       if (url == 'https://m.zmxyj.com/login/me.html' && _currentAccountIndex != -1) {
         accountProvider.updateLoginStatus(_currentAccountIndex, true);
+        await Future.delayed(Duration(seconds: 1));
+        await _logout();
+        await Future.delayed(Duration(seconds: 1));
+        _accountProcessedController.add(null);
+        print("autoLogin---continue via logged in---${_currentAccountIndex}");
+      } else {
+        // _accountProcessedController.add(null);
+        // print("continue via error");
       }
     }
 
